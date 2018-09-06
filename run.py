fd00f92e923e9326cdf375b49760332ac01fad63
Daniel Casey <dcasey720@gmail.com>
2018-09-06 20:38:52 +0000

    For basic quiz game play

--- run.py
+++ run.py
@@ -1,39 +1,164 @@
 import os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-user_info = {"name": "","game_num": 0}
-data = []
 
+def access_user_data(username):
+    """
+    Module takes in username as input, checks players.json database to see
+    the username has been used before. Either increments the number of games
+    played or adds a new username to the players database.
+    Requires player.json file to contain list, either empty or occupied.
+    """
+    global all_players_data
+    global cur_player_data
+    
+    username = username.lower()
+    past_player = False
+    
+    with open("data/players.json", "r") as json_player_data:
+        all_players_data = json.load(json_player_data)
+        
 
-@app.route('/', methods=["GET", "POST"])
+        for obj in all_players_data:
+            if obj["name"] == username:
+                past_player = True
+                cur_player_data = obj
+                
+        if past_player == False:
+            cur_player_data = {"name": username,"game_num": 0, "cur_question": 1, "attempt": 1, "cur_score": 0, "high_score": 0}
+            all_players_data.append(cur_player_data)
+    
+    dump_all_player_data ()
+    
+def dump_all_player_data (): 
+    """
+    Dumps the player data back into players.json
+    """
+    with open("data/players.json", "w") as json_player_data:           
+        json.dump(all_players_data, json_player_data)
+        
+def get_img(index):
+    """
+    Get the image address for the current question
+    """
+    with open("data/tree_lib.json", "r") as json_quiz_data:
+        quiz_data = json.load(json_quiz_data)
+        
+
+        for obj in quiz_data:
+            if obj["index"] == index:  
+
+                tree_image = obj["tree_image"]
+
+    return tree_image
+
+def get_name(index):
+    """
+    Get the answer for the current question
+    """
+    with open("data/tree_lib.json", "r") as json_quiz_data:
+        quiz_data = json.load(json_quiz_data)
+        
+
+        for obj in quiz_data:
+            if obj["index"] == index:  
+
+                tree_name = obj["tree_name"]
+
+    return tree_name
+    
+def check_answer(index, answer):
+    """
+    Checks if the answer submited is correct
+    """
+    answer = answer.lower()
+    with open("data/tree_lib.json", "r") as json_quiz_data:
+        quiz_data = json.load(json_quiz_data)
+        
+
+        for obj in quiz_data:
+            if obj["index"] == index and obj["tree_name"] == answer:
+                return True
+            elif obj["index"] == index and obj["tree_name"] != answer:
+                return False
+                
+@app.route('/')
 def index():
-    """Home page, accepts username, writes to players.json"""
+    """
+    Home page
+    """
     # should make test for this
-    if request.method == "POST":
-        username = request.form["username"].lower()
-        
-        
-        with open("data/players.json", "r") as json_data:
-            data = json.load(json_data)
-            past_player = False
-            
-            for obj in data:
-                if obj["name"] == username:
-                    obj["game_num"] +=1
-                    past_player = True
-            if past_player == False:
-                user_info = {"name": username,"game_num": 0}
-                data.append(user_info)
-        
-        with open("data/players.json", "w") as json_data:           
-            json.dump(data, json_data)
-                        
-            
     return render_template("index.html")
 
 
+@app.route('/start/', methods=['GET', 'POST'])
+def start():
+    """
+    Accepts username entry, accesses/ creates the user info and 
+    initiates quiz
+    """
+    username = request.form["username"]
+    if request.method == "POST" and username != "":
+        
+        # If new user creates info space
+        # If returning user access info
+        access_user_data(username)
+        cur_question = cur_player_data["cur_question"]
+        
+        # Gather the starting quiz info
+        tree_name = get_name(cur_question)
+        tree_image = get_img(cur_question)
+        return render_template("quiz.html", tree_image=tree_image, tree_name=tree_name)
+        
+        
+    else:    
+        return render_template("index.html")   
+        
+        
+@app.route('/submit_answer/', methods=['GET', 'POST'])
+def quiz():
+    """
+    Accepts answer to current question
+    If correct moves onto next question
+    If incorrect does nothing
+    """
+    cur_question = cur_player_data["cur_question"]
+    
+    if request.method == "POST":
+        answer = request.form["answer"]
+        
+        # If correct and more questions, move onto next question
+        if check_answer(cur_question, answer) == True and cur_question < 10:
+            cur_player_data["cur_question"] += 1
+            cur_question = cur_player_data["cur_question"]
+            tree_name = get_name(cur_question)
+            tree_image = get_img(cur_question)
+            dump_all_player_data ()
+            return render_template("quiz.html", tree_image=tree_image, tree_name=tree_name)
+        
+        # If correct and last questions, exit game
+        elif check_answer(cur_question, answer) == True and cur_question == 10:
+            cur_player_data["cur_question"] = 1
+            cur_player_data["game_num"] += 1
+            dump_all_player_data ()
+            return render_template("game_over.html")
+        
+        # If not correct, reload page
+        else:
+            cur_question = cur_player_data["cur_question"]
+            tree_name = get_name(cur_question)
+            tree_image = get_img(cur_question)
+            return render_template("quiz.html", tree_image=tree_image, tree_name=tree_name)
+
+@app.route('/home/', methods=['GET', 'POST'])
+def go_home(): 
+    """
+    Redirects to home page
+    """
+    return render_template("index.html")
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
